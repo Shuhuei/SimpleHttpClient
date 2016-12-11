da843b45849b982136dc3482b6397c6c37de014c
shuhuei <shumach5@gmail.com>
2016-12-11 23:15:31 +0900

    Initial commit

--- SimpleHttpClient/Program.cs
+++ SimpleHttpClient/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json.Linq;
+using System.Web;
 
 namespace SimpleHttpClient
 {
@@ -10,6 +14,30 @@ namespace SimpleHttpClient
     {
         static void Main(string[] args)
         {
+            HttpClient httpClient = new HttpClient();
+            var acceptHeader =
+                new MediaTypeWithQualityHeaderValue("application/json");
+            httpClient.DefaultRequestHeaders.Accept.Add(acceptHeader);
+            httpClient.DefaultRequestHeaders.Authorization
+              = new AuthenticationHeaderValue("Bearer", "token data");
+            httpClient.DefaultRequestHeaders.Add("Header_key", "Header value");
+            HttpResponseMessage httpRes = httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts").Result;
+
+            if (httpRes.IsSuccessStatusCode)
+            {
+                var strRes = httpRes.Content.ReadAsStringAsync().Result;
+                JArray jValue = JArray.Parse(strRes);
+
+                //                JObject jRes = JObject.Parse(strRes);
+                //                JArray jValue = (JArray)jRes["value"];
+
+                foreach (JObject jItem in jValue)
+                {
+                    Console.WriteLine($"Id={((JValue)jItem["id"]).Value},Title = {((JValue)jItem["title"]).Value}");
+                }
+            }
+
+            Console.Read();
         }
     }
 }
